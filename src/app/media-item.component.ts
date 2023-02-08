ef895a670334d9865eae2ca08488824307113220
martin.toman <martin.toman@twentysixdigital.com>
2023-02-08 15:36:53 +0100

    testing property binding

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'mw-media-item',
-  templateUrl: './media-item.component.html',
-  styleUrls: ['./media-item.component.css']
+  selector: "mw-media-item",
+  templateUrl: "./media-item.component.html",
+  styleUrls: ["./media-item.component.scss"],
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name = "The Redemption";
+  wasWatched() {
+    return true;
+  }
+}
